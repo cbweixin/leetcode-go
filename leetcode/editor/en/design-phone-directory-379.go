8a3f34a05d9fdd206378c39f7d75a1b1488d724f
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2020-10-27 11:27:31 -0700

    add another method

--- leetcode/editor/en/design-phone-directory-379.go
+++ leetcode/editor/en/design-phone-directory-379.go
@@ -1,6 +1,6 @@
 package main
 
-type PhoneDirectory struct {
+type PhoneDirectory1 struct {
 	Availables   *Node
 	NoAvailables *Node
 }
@@ -10,17 +10,17 @@ type Node struct {
 	Next   *Node
 }
 
-func Constructor(maxNumbers int) PhoneDirectory {
+func Constructor1(maxNumbers int) PhoneDirectory1 {
 	root := &Node{Number: 0}
 	node := root
 	for i := 1; i < maxNumbers; i++ {
 		node.Next = &Node{Number: i}
 		node = node.Next
 	}
-	return PhoneDirectory{Availables: root}
+	return PhoneDirectory1{Availables: root}
 }
 
-func (this *PhoneDirectory) Get() int {
+func (this *PhoneDirectory1) Get1() int {
 	if this.Availables == nil {
 		return -1
 	}
@@ -31,7 +31,7 @@ func (this *PhoneDirectory) Get() int {
 	return this.NoAvailables.Number
 }
 
-func (this *PhoneDirectory) Check(number int) bool {
+func (this *PhoneDirectory1) Check1(number int) bool {
 	node := this.Availables
 	for node != nil {
 		if node.Number == number {
@@ -42,7 +42,7 @@ func (this *PhoneDirectory) Check(number int) bool {
 	return false
 }
 
-func (this *PhoneDirectory) Release(number int) {
+func (this *PhoneDirectory1) Release1(number int) {
 	var prev *Node
 	node := this.NoAvailables
 	for node != nil {
@@ -58,6 +58,46 @@ func (this *PhoneDirectory) Release(number int) {
 		}
 		prev = node
 		node = node.Next
+	}
+}
+
+type PhoneDirectory struct {
+	dir  []bool
+	free []int
+}
+
+/** Initialize your data structure here
+  @param maxNumbers - The maximum numbers that can be stored in the phone directory. */
+func Constructor(maxNumbers int) PhoneDirectory {
+	free := make([]int, maxNumbers)
+	for i := range free {
+		free[i] = len(free) - 1 - i
+	}
+	return PhoneDirectory{make([]bool, maxNumbers), free}
+}
+
+/** Provide a number which is not assigned to anyone.
+  @return - Return an available number. Return -1 if none is available. */
+func (this *PhoneDirectory) Get() int {
+	if len(this.free) == 0 {
+		return -1
+	}
+	n := this.free[len(this.free)-1]
+	this.free = this.free[:len(this.free)-1]
+	this.dir[n] = true
+	return n
+}
+
+/** Check if a number is available or not. */
+func (this *PhoneDirectory) Check(number int) bool {
+	return !this.dir[number]
+}
+
+/** Recycle or release a number. */
+func (this *PhoneDirectory) Release(number int) {
+	if this.dir[number] {
+		this.dir[number] = false
+		this.free = append(this.free, number)
 	}
 }
 
