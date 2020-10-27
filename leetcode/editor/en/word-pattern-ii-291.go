75690b70bb2556d4d17f8483dd3f313ce01aef0a
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2020-10-27 10:23:22 -0700

    Details Runtime: 64 ms, faster than 33.33% of Go online submissions for Word Pattern II. Memory Usage: 2 MB, less than 100.00% of Go online submissions for Word Pattern II. Next challenges:

---
+++ leetcode/editor/en/word-pattern-ii-291.go
@@ -0,0 +1,44 @@
+package main
+
+func wordPatternMatch(pattern string, str string) bool {
+	p2s := make(map[byte]string)
+	s2p := make(map[string]byte)
+
+	var dfs func(int, int) bool
+	dfs = func(p int, s int) bool {
+		if p == len(pattern) && s == len(str) {
+			return true
+		}
+		if p == len(pattern) || s == len(str) {
+			return false
+		}
+		for i := s; i < len(str); i++ {
+			w, c := str[s:i+1], pattern[p]
+			_, ok1 := s2p[w]
+			v2, ok2 := p2s[c]
+
+			if !ok1 && !ok2 {
+				p2s[c] = w
+				s2p[w] = c
+				if dfs(p+1, i+1) {
+					return true
+				}
+				// backtrack
+				delete(p2s, c)
+				delete(s2p, w)
+			} else if ok2 && v2 == w {
+				return dfs(p+1, i+1)
+			}
+		}
+
+		return false
+
+	}
+
+	return dfs(0, 0)
+
+}
+
+func main() {
+
+}
