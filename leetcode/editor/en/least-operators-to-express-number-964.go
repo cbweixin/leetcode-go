a71d6e7cd21a5c5954d31e32ee804f5fc41aa7db
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2020-10-30 12:05:21 -0700

    12:04 PM	info 				Wrong Answer: 				input:5 				501 				Output:6 				Expected:8 				stdout:

--- leetcode/editor/en/least-operators-to-express-number-964.go
+++ leetcode/editor/en/least-operators-to-express-number-964.go
@@ -1,75 +1,78 @@
 package main
 
-import "math"
+import (
+	"fmt"
+	"math"
+)
 
 //Given a single positive integer x, we will write an expression of the form x (
 //op1) x (op2) x (op3) x ... where each operator op1, op2, etc. is either addition
-//, subtraction, multiplication, or division (+, -, *, or /). For example, with x 
-//= 3, we might write 3 * 3 / 3 + 3 - 3 which is a value of 3. 
+//, subtraction, multiplication, or division (+, -, *, or /). For example, with x
+//= 3, we might write 3 * 3 / 3 + 3 - 3 which is a value of 3.
 //
-// When writing such an expression, we adhere to the following conventions: 
+// When writing such an expression, we adhere to the following conventions:
 //
-// 
-// The division operator (/) returns rational numbers. 
-// There are no parentheses placed anywhere. 
+//
+// The division operator (/) returns rational numbers.
+// There are no parentheses placed anywhere.
 // We use the usual order of operations: multiplication and division happens bef
-//ore addition and subtraction. 
+//ore addition and subtraction.
 // It's not allowed to use the unary negation operator (-). For example, "x - x"
-// is a valid expression as it only uses subtraction, but "-x + x" is not because 
-//it uses negation. 
-// 
+// is a valid expression as it only uses subtraction, but "-x + x" is not because
+//it uses negation.
 //
-// We would like to write an expression with the least number of operators such 
+//
+// We would like to write an expression with the least number of operators such
 //that the expression equals the given target. Return the least number of operator
-//s used. 
+//s used.
 //
-// 
 //
-// 
-// Example 1: 
 //
-// 
+//
+// Example 1:
+//
+//
 //Input: x = 3, target = 19
 //Output: 5
 //Explanation: 3 * 3 + 3 * 3 + 3 / 3.  The expression contains 5 operations.
-// 
 //
-// Example 2: 
 //
-// 
-// 
+// Example 2:
+//
+//
+//
 //Input: x = 5, target = 501
 //Output: 8
 //Explanation: 5 * 5 * 5 * 5 - 5 * 5 * 5 + 5 / 5.  The expression contains 8 ope
 //rations.
-// 
 //
-// 
-// Example 3: 
 //
-// 
+//
+// Example 3:
+//
+//
 //Input: x = 100, target = 100000000
 //Output: 3
-//Explanation: 100 * 100 * 100 * 100.  The expression contains 3 operations. 
+//Explanation: 100 * 100 * 100 * 100.  The expression contains 3 operations.
 //
-// 
-// 
-// 
-// 
 //
-// Note: 
 //
-// 
-// 2 <= x <= 100 
-// 1 <= target <= 2 * 10^8 
-// 
 //
-// 
-// 
-// 
-// 
-// 
-// Related Topics Math Dynamic Programming 
+//
+//
+// Note:
+//
+//
+// 2 <= x <= 100
+// 1 <= target <= 2 * 10^8
+//
+//
+//
+//
+//
+//
+//
+// Related Topics Math Dynamic Programming
 // 👍 190 👎 50
 
 // 2020-10-30 11:47:10
@@ -96,15 +99,15 @@ func leastOpsExpressTarget(x int, target int) int {
 			return 0
 		}
 
-		if x > goal{
-			memo[goal] = min(goal * 2 - 1, (x-goal)*2)
+		if x > goal {
+			memo[goal] = min(goal*2-1, (x-goal)*2)
 			return memo[goal]
 		}
 
 		sums, cnt := x, 0
 		for sums < goal {
-			sums *= goal
-			cnt ++
+			sums *= x
+			cnt++
 		}
 
 		if sums == goal {
@@ -113,12 +116,12 @@ func leastOpsExpressTarget(x int, target int) int {
 		}
 
 		l, r := math.MaxInt32, math.MaxInt32
-		if sums - goal < goal {
+		if sums-goal < goal {
 			l = dfs(sums-goal) + cnt
 		}
-		r = dfs(int(sums/goal)) + cnt +1
+		r = dfs(int(sums/goal)) + cnt + 1
 
-		memo[goal] = min(l,r) + 1
+		memo[goal] = min(l, r) + 1
 		return memo[goal]
 	}
 
@@ -126,3 +129,6 @@ func leastOpsExpressTarget(x int, target int) int {
 }
 
 //leetcode submit region end(Prohibit modification and deletion)
+func main() {
+	fmt.Println(leastOpsExpressTarget(3, 19))
+}
