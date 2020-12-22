4eff19ea46d7aeb75a64293a0b5f0f9b94c498ea
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2020-12-22 10:22:09 -0800

    add new files

---
+++ reverse-slice_test.go
@@ -0,0 +1,16 @@
+package main
+
+import "testing"
+
+type myType int
+
+func TestReverse(t *testing.T) {
+	var slice = []myType{0, 1, 2}
+	Reverse(slice)
+	// ptr can not pass the test
+	//Reverse(&slice)
+	if slice[0] != myType(2) || slice[1] != myType(1) || slice[2] != myType(0) || len(slice) != 3 {
+		t.Fail()
+	}
+
+}
