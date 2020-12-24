49f6acd7c71b9ffadb1878f496346455932a849a
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2020-12-23 21:44:57 -0800

    add new files

---
+++ time.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+var week time.Duration
+
+func main() {
+	t := time.Now()
+	fmt.Println(t)
+	fmt.Printf("%02d.%02d.%4d\n", t.Day(), t.Month(), t.Year())
+	t = time.Now().UTC()
+	fmt.Println(t)
+	// calculating times:
+	week = 60 * 60 * 24 * 7 * 1e9 // must be in nanosec
+	week_from_now := t.Add(time.Duration(week))
+	fmt.Println(week_from_now) // Wed Dec 28 08:52:14 +0000 UTC 2011
+	// formatting times:
+	fmt.Println(t.Format(time.RFC822)) // 21 Dec 11 0852 UTC
+	fmt.Println(t.Format(time.ANSIC))  // Wed Dec 21 08:56:34 2011
+	// The time must be 2006-01-02 15:04:05
+	fmt.Println(t.Format("02 Jan 2006 15:04")) // 21 Dec 2011 08:52
+	s := t.Format("20060102")
+	fmt.Println(t, "=>", s)
+}
