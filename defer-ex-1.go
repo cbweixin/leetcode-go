8479ccaaa21b6a4b55369dbde695de239903c191
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2021-01-01 10:27:28 -0800

    add new files

--- defer-ex-1.go
+++ defer-ex-1.go
@@ -10,8 +10,23 @@ func function1() {
 	fmt.Printf("In function1 at the top\n")
 	defer function2()
 	fmt.Printf("In function1 at the bottom!\n")
+	a()
+	f()
 }
 
 func function2() {
 	fmt.Printf("Function2: Deferred until the end of the calling function!")
+}
+
+func a() {
+	i := 0
+	defer fmt.Println(i)
+	i++
+	return
+}
+
+func f() {
+	for i := 0; i < 5; i++ {
+		defer fmt.Printf("%d ", i)
+	}
 }
