94c63c288adbf6161b176d3edae485d26ffcfe28
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2021-01-01 10:28:48 -0800

    add new files

--- defer-ex-1.go
+++ defer-ex-1.go
@@ -7,6 +7,7 @@ func main() {
 }
 
 func function1() {
+	doDBOperations()
 	fmt.Printf("In function1 at the top\n")
 	defer function2()
 	fmt.Printf("In function1 at the bottom!\n")
@@ -27,6 +28,26 @@ func a() {
 
 func f() {
 	for i := 0; i < 5; i++ {
-		defer fmt.Printf("%d ", i)
+		defer fmt.Printf("%d \n", i)
 	}
+}
+
+func connectToDB() {
+	fmt.Println("ok, connected to db")
+}
+
+func disconnectFromDB() {
+	fmt.Println("ok, disconnected from db")
+}
+
+func doDBOperations() {
+	connectToDB()
+	fmt.Println("Defering the database disconnect.")
+	defer disconnectFromDB() //function called here with defer
+	fmt.Println("Doing some DB operations ...")
+	fmt.Println("Oops! some crash or network error ...")
+	fmt.Println("Returning from function here!")
+	return //terminate the program
+	// deferred function executed here just before actually returning, even if
+	// there is a return or abnormal termination before
 }
