b3ad9a19ecf5288207fec76d73a26f0164161d51
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2021-01-01 11:08:03 -0800

    add new files

--- defer-tracing.go
+++ defer-tracing.go
@@ -4,10 +4,31 @@ import "fmt"
 
 func main() {
 	b()
+	bb()
 }
 
 func trace(s string)   { fmt.Println("entering:", s) }
 func untrace(s string) { fmt.Println("leaving:", s) }
+
+func trace1(s string) string {
+	fmt.Println("entering:", s)
+	return s
+}
+
+func un(s string) {
+	fmt.Println("leaving:", s)
+}
+
+func ab() {
+	defer un(trace1("ab"))
+	fmt.Println("in ab")
+}
+
+func bb() {
+	defer un(trace1("bb"))
+	fmt.Println("in bb")
+	ab()
+}
 
 func aa() {
 	trace("aa")
