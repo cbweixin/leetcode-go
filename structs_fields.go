c46cfa848891d40bcb8034f05f19aadd76367a7c
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2021-01-02 15:48:25 -0800

    add new files

--- structs_fields.go
+++ structs_fields.go
@@ -1,11 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 type struct1 struct {
 	i1  int
 	f1  float32
 	str string
+}
+
+type Person struct {
+	firstName string
+	lastName  string
+}
+
+func upPerson(p *Person) {
+	p.firstName = strings.ToUpper(p.firstName)
+	p.lastName = strings.ToUpper(p.lastName)
 }
 
 func main() {
@@ -18,4 +31,25 @@ func main() {
 	fmt.Printf("The float is: %f\n", ms.f1)
 	fmt.Printf("The string is: %s\n", ms.str)
 	fmt.Println(ms)
+
+	// 1-struct as a value type:
+	var pers1 Person
+	pers1.firstName = "Chris"
+	pers1.lastName = "Woodward"
+	upPerson(&pers1)
+	fmt.Printf("The name of the person is %s %s\n", pers1.firstName, pers1.lastName)
+
+	// 2—struct as a pointer:
+	pers2 := new(Person)
+	pers2.firstName = "Chris"
+	pers2.lastName = "Woodward1"
+	(*pers2).lastName = "Woodward" // 这是合法的
+	upPerson(pers2)
+	fmt.Printf("The name of the person is %s %s\n", pers2.firstName, pers2.lastName)
+
+	// 3—struct as a literal:
+	pers3 := &Person{"Chris", "Woodward"}
+	upPerson(pers3)
+	fmt.Printf("The name of the person is %s %s\n", pers3.firstName, pers3.lastName)
+
 }
