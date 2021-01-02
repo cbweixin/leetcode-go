fcce5d0191b71fe0daaee8551d2a586b25010ed8
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2021-01-02 13:43:04 -0800

    add new files

---
+++ make_maps.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+
+func main() {
+	var mapLit map[string]int
+	//var mapCreated map[string]float32
+	var mapAssigned map[string]int
+
+	mapLit = map[string]int{"one": 1, "two": 2}
+	mapCreated := make(map[string]float32)
+	mapAssigned = mapLit
+
+	mapCreated["key1"] = 4.5
+	mapCreated["key2"] = 3.14159
+	mapAssigned["two"] = 3
+
+	fmt.Printf("Map literal at \"one\" is: %d\n", mapLit["one"])
+	fmt.Printf("Map created at \"key2\" is: %f\n", mapCreated["key2"])
+	fmt.Printf("Map assigned at \"two\" is: %d\n", mapLit["two"])
+	fmt.Printf("Map literal at \"ten\" is: %d\n", mapLit["ten"])
+
+	var value int
+	var isPresent bool
+
+	map1 := make(map[string]int)
+	map1["New Delhi"] = 55
+	map1["Beijing"] = 20
+	map1["Washington"] = 25
+	value, isPresent = map1["Beijing"]
+	if isPresent {
+		fmt.Printf("The value of \"Beijing\" in map1 is: %d\n", value)
+	} else {
+		fmt.Printf("map1 does not contain Beijing")
+	}
+
+	value, isPresent = map1["Paris"]
+	fmt.Printf("Is \"Paris\" in map1 ?: %t\n", isPresent)
+	fmt.Printf("Value is: %d\n", value)
+
+	// delete an item:
+	delete(map1, "Washington")
+	value, isPresent = map1["Washington"]
+	if isPresent {
+		fmt.Printf("The value of \"Washington\" in map1 is: %d\n", value)
+	} else {
+		fmt.Println("map1 does not contain Washington")
+	}
+
+}
