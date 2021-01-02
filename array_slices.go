cb032b51a451aacbfa7c12bfee02886b721b959b
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2021-01-02 10:27:47 -0800

    add new files

--- array_slices.go
+++ array_slices.go
@@ -36,6 +36,21 @@ func main() {
 	// compile error, cannot use [5] int as [] int
 	//sum_slice(arr)
 	sum_slice(arr[:])
+	// two additional ways to create slices
+	// make([]int, 50, 100)
+	// new([100]int)[0:50]
+	var slice2 []int = make([]int, 10)
+	// load the array/slice:
+	for i := 0; i < len(slice1); i++ {
+		slice2[i] = 5 * i
+	}
+
+	// print the slice:
+	for i := 0; i < len(slice1); i++ {
+		fmt.Printf("Slice at %d is %d\n", i, slice1[i])
+	}
+	fmt.Printf("\nThe length of slice1 is %d\n", len(slice1))
+	fmt.Printf("The capacity of slice1 is %d\n", cap(slice1))
 
 }
 
