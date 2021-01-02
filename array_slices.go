c1b842c931ca3370641b76d76b40b929ee424330
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2021-01-02 13:21:05 -0800

    add new files

--- array_slices.go
+++ array_slices.go
@@ -87,6 +87,17 @@ func main() {
 		fmt.Printf("Slice at %d is %d\n", i, slice4[i])
 	}
 
+	slFrom := []int{1, 2, 3}
+	slTo := make([]int, 10)
+
+	n := copy(slTo, slFrom)
+	fmt.Println(slTo)
+	fmt.Printf("Copied %d elements\n", n) // n == 3
+
+	sl3 := []int{1, 2, 3}
+	sl3 = append(sl3, 4, 5, 6)
+	fmt.Println(sl3)
+
 }
 
 // a is a slice, []int, there is no length in '[]'
