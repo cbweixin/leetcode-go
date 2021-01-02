879f0bf2c2a88f6b585355e6c0cc6b7e5f75e158
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2021-01-02 14:02:42 -0800

    add new files

--- make_maps.go
+++ make_maps.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
 	var mapLit map[string]int
@@ -45,6 +48,63 @@ func main() {
 		fmt.Printf("The value of \"Washington\" in map1 is: %d\n", value)
 	} else {
 		fmt.Println("map1 does not contain Washington")
+	}
+
+	map2 := make(map[int]float32)
+	map2[1] = 1.0
+	map2[2] = 2.0
+	map2[3] = 3.0
+	map2[4] = 4.0
+	for key, value := range map2 {
+		fmt.Printf("key is: %d - value is: %f\n", key, value)
+	}
+
+	// Version A:
+	items := make([]map[int]int, 5)
+	for i := range items {
+		items[i] = make(map[int]int, 1)
+		items[i][1] = 2
+	}
+	fmt.Printf("Version A: Value of items: %v\n", items)
+
+	// Version B: NOT GOOD!
+	items2 := make([]map[int]int, 5)
+	for _, item := range items2 {
+		item = make(map[int]int, 1) // item is only a copy of the slice element.
+		item[1] = 2                 // This 'item' will be lost on the next iteration.
+	}
+	fmt.Printf("Version B: Value of items: %v\n", items2)
+
+	var (
+		barVal = map[string]int{"alpha": 34, "bravo": 56, "charlie": 23,
+			"delta": 87, "echo": 56, "foxtrot": 12,
+			"golf": 34, "hotel": 16, "indio": 87,
+			"juliet": 65, "kili": 43, "lima": 98}
+	)
+	fmt.Println("unsorted:")
+	for k, v := range barVal {
+		fmt.Printf("Key: %v, Value: %v / ", k, v)
+	}
+	keys := make([]string, len(barVal))
+	i := 0
+	for k, _ := range barVal {
+		keys[i] = k
+		i++
+	}
+	sort.Strings(keys)
+	fmt.Println()
+	fmt.Println("sorted:")
+	for _, k := range keys {
+		fmt.Printf("Key: %v, Value: %v / ", k, barVal[k])
+	}
+
+	invMap := make(map[int]string, len(barVal))
+	for k, v := range barVal {
+		invMap[v] = k
+	}
+	fmt.Println("inverted:")
+	for k, v := range invMap {
+		fmt.Printf("Key: %v, Value: %v / ", k, v)
 	}
 
 }
