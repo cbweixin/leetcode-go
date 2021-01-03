02851ed487c5a693887bb9eb30bab020f2f3b1cb
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2021-01-03 13:47:22 -0800

    add new files

---
+++ node_structures.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+type Node struct {
+	le   *Node
+	data interface{}
+	ri   *Node
+}
+
+func NewNode(left, right *Node) *Node {
+	return &Node{left, nil, right}
+}
+
+func (n *Node) SetData(data interface{}) {
+	n.data = data
+}
+
+func main() {
+	root := NewNode(nil, nil)
+	root.SetData("root node")
+	// make child (leaf) nodes:
+	a := NewNode(nil, nil)
+	a.SetData("left node")
+	b := NewNode(nil, nil)
+	b.SetData("right node")
+	root.le = a
+	root.ri = b
+	//&{0xc0000044a0 root node 0xc0000044c0}
+	fmt.Printf("%v\n", root)
+
+}
