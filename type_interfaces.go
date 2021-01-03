95456f3d24f934daafdb1a0773447dc950b84447
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2021-01-03 10:56:48 -0800

    add new files

---
+++ type_interfaces.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Square2 struct {
+	side float32
+}
+
+type Circle2 struct {
+	radius float32
+}
+
+type Shaper2 interface {
+	Area2() float32
+}
+
+func (sq *Square2) Area2() float32 {
+	return sq.side * sq.side
+}
+
+func (ci *Circle2) Area2() float32 {
+	return ci.radius * ci.radius * math.Pi
+}
+
+func main() {
+	var areaIntf Shaper2
+	sq1 := new(Square2)
+	sq1.side = 5
+
+	areaIntf = sq1
+	// Is Square the type of areaIntf?
+	if t, ok := areaIntf.(*Square2); ok {
+		fmt.Printf("The type of areaIntf is: %T\n", t)
+	}
+	if u, ok := areaIntf.(*Circle2); ok {
+		fmt.Printf("The type of areaIntf is: %T\n", u)
+	} else {
+		fmt.Println("areaIntf does not contain a variable of type Circle")
+	}
+
+	var areaIntf2 Shaper2
+	circle1 := new(Circle2)
+	circle1.radius = 5
+
+	areaIntf2 = circle1
+	// Is Square the type of areaIntf?
+	if t, ok := areaIntf2.(*Square2); ok {
+		fmt.Printf("The type of areaIntf is: %T\n", t)
+	}
+	if u, ok := areaIntf2.(*Circle2); ok {
+		fmt.Printf("The type of areaIntf is: %T\n", u)
+	} else {
+		fmt.Println("areaIntf does not contain a variable of type Circle")
+	}
+}
