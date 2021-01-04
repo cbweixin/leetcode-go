2dfb0a903957759e288f44a2f45ba7e7b596ccb6
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2021-01-04 14:29:58 -0800

    add files

---
+++ panics.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func badCall() {
+	panic("bad end")
+}
+
+func test() {
+	defer func() {
+		if e := recover(); e != nil {
+			fmt.Printf("Panicing %s\r\n", e)
+		}
+	}()
+	badCall()
+	fmt.Printf("After bad call\r\n") // <-- wordt niet bereikt
+}
+
+func main() {
+	//fmt.Println("starting programm...")
+	//panic("pacnic oh, oaa")
+	//fmt.Println("hello")
+
+
+	fmt.Printf("Calling test\r\n")
+	test()
+	fmt.Printf("Test completed\r\n")
+	
+}
