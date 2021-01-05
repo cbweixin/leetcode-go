5b4357d808c38824ad22f2dd971e7686778eb0d9
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2021-01-05 14:28:34 -0800

    add new file

--- goroutine_panic.go
+++ goroutine_panic.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func tel(ch chan int) {
@@ -9,6 +10,13 @@ func tel(ch chan int) {
 		ch <- i
 	}
 	close(ch) // if this is ommitted: panic: all goroutines are asleep - deadlock!
+}
+
+func tel2(ch chan int, quit chan bool) {
+	for i := 0; i < 15; i++ {
+		ch <- i
+	}
+	quit <- true
 }
 
 func main() {
@@ -20,6 +28,21 @@ func main() {
 	for ok {
 		if i, ok = <-ch; ok {
 			fmt.Printf("ok is %t and the counter is at %d\n", ok, i)
+		}
+	}
+
+	var ok2 = true
+	quit := make(chan bool)
+	ch2 := make(chan int)
+	go tel2(ch2, quit)
+
+	for ok2 {
+		select {
+		case i := <-ch2:
+			fmt.Printf("the counter is %d\n", i)
+
+		case <-quit:
+			os.Exit(0)
 		}
 	}
 
