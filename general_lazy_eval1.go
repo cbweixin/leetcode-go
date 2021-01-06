e74800a06646a7ae55a3bc57bcd11de06aad0bd1
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2021-01-06 05:57:42 -0800

    add new file

--- general_lazy_eval1.go
+++ general_lazy_eval1.go
@@ -29,6 +29,19 @@ func main() {
 	for i := 0; i < 10; i++ {
 		fmt.Printf("%vth even: %v\n", i, s())
 	}
+
+	fibFunc := func(state Any) (Any, Any) {
+		os := state.([]uint64)
+		v1 := os[0]
+		v2 := os[1]
+		ns := []uint64{v2, v1 + v2}
+		return v1, ns
+	}
+	fib := BuildLazyIntEvaluator_fib(fibFunc, []uint64{0, 1})
+	for i := 0; i < 10; i++ {
+		fmt.Printf("Fib nr %v: %v\n", i, fib())
+	}
+
 }
 
 func BuildLazyEvaluator(evalFunc EvalFunc, initState Any) func() Any {
@@ -59,5 +72,12 @@ func BuildLazyIntEvaluator2(evalFunc EvalFunc, initState Any) func() string {
 	ef := BuildLazyEvaluator(evalFunc, initState)
 	return func() string {
 		return ef().(string)
+	}
+}
+
+func BuildLazyIntEvaluator_fib(evalFunc EvalFunc, initState Any) func() uint64 {
+	ef := BuildLazyEvaluator(evalFunc, initState)
+	return func() uint64 {
+		return ef().(uint64)
 	}
 }
