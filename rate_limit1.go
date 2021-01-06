c21b601e33be6b345951c7e9d99bada76d20fe5f
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2021-01-06 07:06:11 -0800

    add new file

---
+++ rate_limit1.go
@@ -0,0 +1,34 @@
+package main
+
+const MAXREQS = 50
+
+var sem = make(chan int, MAXREQS)
+
+type Request3 struct {
+	a, b   int
+	replyc chan int
+}
+
+func process(r *Request3) {
+	// do something
+}
+
+func handle(r *Request3) {
+	sem <- 1 // doesn't matter what we put in it
+	process(r)
+	<-sem // one empty place in the buffer: the next request can start
+}
+
+func server4(service chan *Request3) {
+	for {
+		request := <-service
+		go handle(request)
+	}
+}
+
+func main() {
+
+	service := make(chan *Request3)
+	go server4(service)
+
+}
