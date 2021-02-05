703f27420708db04e88604a42ee763ea5ad6fc9d
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2021-02-05 14:27:44 -0800

    Wrong Answer: input:[2,3,3,4,6,7] 12 Output:60 Expected:61 stdout:

---
+++ leetcode/editor/en/number-of-subsequences-that-satisfy-the-given-sum-condition-1498.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"math"
+	"sort"
+)
+
+//Given an array of integers nums and an integer target.
+//
+// Return the number of non-empty subsequences of nums such that the sum of the
+//minimum and maximum element on it is less or equal to target. Since the answer m
+//ay be too large, return it modulo 109 + 7.
+//
+//
+// Example 1:
+//
+//
+//Input: nums = [3,5,6,7], target = 9
+//Output: 4
+//Explanation: There are 4 subsequences that satisfy the condition.
+//[3] -> Min value + max value <= target (3 + 3 <= 9)
+//[3,5] -> (3 + 5 <= 9)
+//[3,5,6] -> (3 + 6 <= 9)
+//[3,6] -> (3 + 6 <= 9)
+//
+//
+// Example 2:
+//
+//
+//Input: nums = [3,3,6,8], target = 10
+//Output: 6
+//Explanation: There are 6 subsequences that satisfy the condition. (nums can ha
+//ve repeated numbers).
+//[3] , [3] , [3,3], [3,6] , [3,6] , [3,3,6]
+//
+// Example 3:
+//
+//
+//Input: nums = [2,3,3,4,6,7], target = 12
+//Output: 61
+//Explanation: There are 63 non-empty subsequences, two of them don't satisfy th
+//e condition ([6,7], [7]).
+//Number of valid subsequences (63 - 2 = 61).
+//
+//
+// Example 4:
+//
+//
+//Input: nums = [5,2,4,1,7,6,8], target = 16
+//Output: 127
+//Explanation: All non-empty subset satisfy the condition (2^7 - 1) = 127
+//
+//
+// Constraints:
+//
+//
+// 1 <= nums.length <= 105
+// 1 <= nums[i] <= 106
+// 1 <= target <= 106
+//
+// Related Topics Sort Sliding Window
+// 👍 478 👎 47
+
+// 2021-02-05 14:18:48
+
+//leetcode submit region begin(Prohibit modification and deletion)
+func numSubseq(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+	res, MOD := 0, int(math.Pow10(9))+7
+	sort.Ints(nums)
+
+	for left < right {
+		if nums[left]+nums[right] > target {
+			right -= 1
+		} else {
+			res += int(math.Pow(float64(2), float64(right-left))) % MOD
+			left++
+		}
+	}
+
+	return res % MOD
+
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
