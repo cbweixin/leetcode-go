2a30c0e6fed7aba70507a4987f544c19263e3057
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2021-02-15 10:43:24 -0800

    wrong, the first test case gives 6 instead of 5

---
+++ leetcode/editor/en/missing-element-in-sorted-array-1060.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func missingElement(nums []int, k int) int {
+	l := len(nums)
+	left, right := 0, l-1
+	for left < right {
+		mid := (left + right + 1) >> 1
+		if nums[mid]-nums[0]-mid < k {
+			left = mid
+		} else {
+			right = mid - 1
+		}
+
+	}
+
+	return nums[0] + k + 1
+
+}
+
+func main() {
+	fmt.Println(missingElement([]int{4, 7, 9, 10}, 1))
+
+}
