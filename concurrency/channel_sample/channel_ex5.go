6705037058a75524617f1b3d81b015d81a78c484
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2021-02-18 11:27:17 -0800

    add some new files

---
+++ concurrency/channel_sample/channel_ex5.go
@@ -0,0 +1,42 @@
+package channel_sample
+
+import (
+	"fmt"
+	"time"
+)
+
+var mapChan3 = make(chan map[string]*Counter, 1)
+
+func Chan_With_Map3() {
+	syncChan := make(chan struct{}, 2)
+	go func() { // 用于演示接收操作。
+		for {
+			if elem, ok := <-mapChan3; ok {
+				counter := elem["count"]
+				counter.count++
+			} else {
+				break
+			}
+		}
+		fmt.Println("Stopped. [receiver]")
+		syncChan <- struct{}{}
+	}()
+	go func() { // 用于演示发送操作。
+		countMap := map[string]*Counter{
+			"count": {},
+		}
+		for i := 0; i < 5; i++ {
+			mapChan3 <- countMap
+			time.Sleep(time.Millisecond)
+			fmt.Printf("The count map: %v. [sender]\n", countMap)
+		}
+		close(mapChan3)
+		syncChan <- struct{}{}
+	}()
+	<-syncChan
+	<-syncChan
+}
+
+func (counter *Counter) String() string {
+	return fmt.Sprintf("{count : %d}", counter.count)
+}
