8cfa8d67d4850eb7a481b621684b11fab6ce9040
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2021-02-18 07:59:38 -0800

    add some new files

---
+++ concurrency/channel_sample/channel_ex3.go
@@ -0,0 +1,36 @@
+package channel_sample
+
+import (
+	"fmt"
+	"time"
+)
+
+var mapChan = make(chan map[string]int, 1)
+
+func Chan_With_Map() {
+	syncChan := make(chan struct{}, 2)
+	go func() { // 用于演示接收操作。
+		for {
+			if elem, ok := <-mapChan; ok {
+				elem["count"]++
+			} else {
+				break
+			}
+		}
+		fmt.Println("Stopped. [receiver]")
+		syncChan <- struct{}{}
+	}()
+	go func() { // 用于演示发送操作。
+		countMap := make(map[string]int)
+		for i := 0; i < 5; i++ {
+			mapChan <- countMap
+			time.Sleep(time.Millisecond)
+			fmt.Printf("The count map: %v. [sender]\n", countMap)
+		}
+		close(mapChan)
+		syncChan <- struct{}{}
+	}()
+	<-syncChan
+	<-syncChan
+
+}
