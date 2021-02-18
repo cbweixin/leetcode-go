e502c5916dd4767994c41bb5114c1846bef9a4c7
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2021-02-17 20:17:08 -0800

    add some comments

--- concur_go_1.go
+++ concur_go_1.go
@@ -17,6 +17,7 @@ func main() {
 	// name as an variable would be assigned to value 'Eric', .. 'Mark',because for loop is fast
 	// also the time to run goroutine is not determinstic, so goroutine runs, very possibly the name
 	// already has value Mark
+	// if you want each name for each goroutine, see concur_go_2.go file
 	for _, name := range names {
 		go func() {
 			fmt.Printf("Hello, %s!\n", name)
