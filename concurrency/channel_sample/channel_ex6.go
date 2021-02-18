ec9e8b3b21befc43f93c43387363411a4e3b4ca0
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2021-02-18 11:27:41 -0800

    add some new files

---
+++ concurrency/channel_sample/channel_ex6.go
@@ -0,0 +1,35 @@
+package channel_sample
+
+import "fmt"
+
+func Close_test() {
+	dataChan := make(chan int, 5)
+	syncChan1 := make(chan struct{}, 1)
+	syncChan2 := make(chan struct{}, 2)
+	go func() { // 用于演示接收操作。
+		<-syncChan1
+		for {
+			if elem, ok := <-dataChan; ok {
+				fmt.Printf("Received: %d [receiver]\n", elem)
+			} else {
+				break
+			}
+		}
+		fmt.Println("Done. [receiver]")
+		syncChan2 <- struct{}{}
+	}()
+	go func() { // 用于演示发送操作。
+		for i := 0; i < 5; i++ {
+			dataChan <- i
+			fmt.Printf("Sent: %d [sender]\n", i)
+		}
+		// firstly close the channel
+		close(dataChan)
+		// then start the receiver, but receiver still able to get all element in channel
+		syncChan1 <- struct{}{}
+		fmt.Println("Done. [sender]")
+		syncChan2 <- struct{}{}
+	}()
+	<-syncChan2
+	<-syncChan2
+}
