ef3913385d00138e0bbd569ec80a7548c94396b0
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2021-02-18 16:36:04 -0800

    add some new files

--- concurrency/channel_sample/channel_ex12.go
+++ concurrency/channel_sample/channel_ex12.go
@@ -3,16 +3,26 @@ package channel_sample
 import "fmt"
 
 func Select_test3() {
-	chanCap := 5
-	intChan := make(chan int, chanCap)
-	for i := 0; i < chanCap; i++ {
-		select {
-		case intChan <- 1:
-		case intChan <- 2:
-		case intChan <- 3:
+	intChan := make(chan int, 10)
+	for i := 0; i < 10; i++ {
+		intChan <- i
+	}
+	close(intChan)
+	syncChan := make(chan struct{}, 1)
+	go func() {
+	Loop:
+		for {
+			select {
+			case e, ok := <-intChan:
+				if !ok {
+					fmt.Println("End.")
+					// be aware of the tag LOOP , without it, you just break `select` instead of `for` loop
+					break Loop
+				}
+				fmt.Printf("Received: %v\n", e)
+			}
 		}
-	}
-	for i := 0; i < chanCap; i++ {
-		fmt.Printf("%d\n", <-intChan)
-	}
+		syncChan <- struct{}{}
+	}()
+	<-syncChan
 }
