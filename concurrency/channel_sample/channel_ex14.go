390901255b1f1ce7200ca67c56352f8b6cd78d14
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2021-02-19 08:58:02 -0800

    Success: Runtime:0 ms, faster than 100.00% of Go online submissions. Memory Usage:4.3 MB, less than 28.13% of Go online submissions.

---
+++ concurrency/channel_sample/channel_ex14.go
@@ -0,0 +1,14 @@
+package channel_sample
+
+import (
+	"fmt"
+	"time"
+)
+
+func Timer_test() {
+	timer := time.NewTimer(2 * time.Second)
+	fmt.Printf("Present time: %v.\n", time.Now())
+	expirationTime := <-timer.C
+	fmt.Printf("Expiration time: %v.\n", expirationTime)
+	fmt.Printf("Stop timer: %v.\n", timer.Stop())
+}
