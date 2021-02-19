10c326cc50c32b4ebbf9389b81bc3b5ed38a276b
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2021-02-19 12:13:48 -0800

    add new files

---
+++ concurrency/channel_sample/channel_ex17.go
@@ -0,0 +1,31 @@
+package channel_sample
+
+import (
+	"fmt"
+	"time"
+)
+
+func Ticker_test() {
+	intChan := make(chan int, 1)
+	ticker := time.NewTicker(time.Second)
+	go func() {
+		for range ticker.C {
+			select {
+			case intChan <- 1:
+			case intChan <- 2:
+			case intChan <- 3:
+			}
+		}
+		fmt.Println("End. [sender]")
+	}()
+	var sum int
+	for e := range intChan {
+		fmt.Printf("Received: %v\n", e)
+		sum += e
+		if sum > 10 {
+			fmt.Printf("Got: %v\n", sum)
+			break
+		}
+	}
+	fmt.Println("End. [receiver]")
+}
