c61f93e20a79aeb440f7e2a7cc45fbd9c72cbef0
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2021-02-20 11:37:22 -0800

    make some change

--- concurrency/loadgen/lib/gotickets.go
+++ concurrency/loadgen/lib/gotickets.go
@@ -1,5 +1,10 @@
 package lib
 
+import (
+	"errors"
+	"fmt"
+)
+
 type GoTickets interface {
 	// take on ticket
 	Take()
@@ -20,4 +25,56 @@ type myGotickets struct {
 	ticketCh chan struct{}
 	// itcket is active or not
 	active bool
+}
+
+func (gt *myGotickets) Take() {
+	<-gt.ticketCh
+}
+
+func (gt *myGotickets) Return() {
+	gt.ticketCh <- struct{}{}
+}
+
+func (gt *myGotickets) Active() bool {
+	return gt.active
+}
+
+func (gt *myGotickets) Total() uint32 {
+	return gt.total
+}
+
+func (gt *myGotickets) Remaider() uint32 {
+	return uint32(len(gt.ticketCh))
+}
+
+// create a new goroutine ticket pool
+func NewGoTickets(total uint32) (GoTickets, error) {
+	gt := myGotickets{}
+	if !gt.init(total) {
+		errMsg :=
+			fmt.Sprintf("The goroutine ticket pool can NOT be initialized! (total=%d)\n", total)
+		return nil, errors.New(errMsg)
+	}
+	return &gt, nil
+}
+
+func (gt *myGotickets) init(total uint32) bool {
+	if gt.active {
+		return false
+	}
+	if total == 0 {
+		return false
+	}
+
+	ch := make(chan struct{}, total)
+	n := int(total)
+	for i := 0; i < n; i++ {
+		ch <- struct{}{}
+	}
+	gt.ticketCh = ch
+	gt.total = total
+	gt.active = true
+
+	return true
+
 }
