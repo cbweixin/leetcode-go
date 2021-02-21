0d0197f174c10d55e9d5afacf2bc8ec457c7ca8c
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2021-02-21 14:31:26 -0800

    add new files

--- concurrency/loadgen/gen.go
+++ concurrency/loadgen/gen.go
@@ -1,3 +1,26 @@
 package loadgen
 
-var logger = log.
+import (
+	"../../helper/log"
+	"./lib"
+	"context"
+	"time"
+)
+
+var logger = log.DLogger()
+
+type myGenerator struct {
+	caller      lib.Caller           // 调用器。
+	timeoutNS   time.Duration        // 处理超时时间，单位：纳秒。
+	lps         uint32               // 每秒载荷量。
+	durationNS  time.Duration        // 负载持续时间，单位：纳秒。
+	concurrency uint32               // 载荷并发量。
+	tickets     lib.GoTickets        // Goroutine票池。
+	ctx         context.Context      // 上下文。
+	cancelFunc  context.CancelFunc   // 取消函数。
+	callCount   int64                // 调用计数。
+	status      uint32               // 状态。
+	resultCh    chan *lib.CallResult // 调用结果通道。
+}
+
+func NewGenerator(pset ParamSet) (lib.Generator, error)
---
+++ concurrency/loadgen/parameter.go
@@ -0,0 +1,52 @@
+package loadgen
+
+import (
+	"./lib"
+	"bytes"
+	"errors"
+	"fmt"
+	"strings"
+	"time"
+)
+
+type ParamSet struct {
+	Caller     lib.Caller           // 调用器。
+	TimeoutNS  time.Duration        // 响应超时时间，单位：纳秒。
+	LPS        uint32               // 每秒载荷量。
+	DurationNS time.Duration        // 负载持续时间，单位：纳秒。
+	ResultCh   chan *lib.CallResult // 调用结果通道。
+}
+
+func (pset *ParamSet) Check() error {
+	var errMsgs []string
+
+	if pset.Caller == nil {
+		errMsgs = append(errMsgs, "Invalid caller!")
+	}
+	if pset.LPS == 0 {
+		errMsgs = append(errMsgs, "Invalid lps(load per second)!")
+	}
+	if pset.TimeoutNS == 0 {
+		errMsgs = append(errMsgs, "Invalid timeoutNS!")
+	}
+	if pset.DurationNS == 0 {
+		errMsgs = append(errMsgs, "Invalid durationNS!")
+	}
+	if pset.ResultCh == nil {
+		errMsgs = append(errMsgs, "Invalid result channel!")
+	}
+
+	var buf bytes.Buffer
+	buf.WriteString("Checking the parameters...")
+	if errMsgs != nil {
+		errMsg := strings.Join(errMsgs, " ")
+		buf.WriteString(fmt.Sprintf("NOT passed! (%s)", errMsg))
+		logger.Infoln(buf.String())
+		return errors.New(errMsg)
+	}
+	buf.WriteString(
+		fmt.Sprintf("Passed. (timeoutNS=%s, lps=%d,durationNS=%s)",
+			pset.TimeoutNS, pset.LPS, pset.DurationNS))
+	logger.Infoln(buf.String())
+	return nil
+}
