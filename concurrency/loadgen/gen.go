43a03940f7a82b5ec1e4dd3493b9577a4d3354c1
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2021-02-21 16:19:04 -0800

    add new files

--- concurrency/loadgen/gen.go
+++ concurrency/loadgen/gen.go
@@ -98,3 +98,168 @@ func (gen *myGenerator) callOne(rawReq *lib.RawReq) *lib.RawResp {
 
 	return &rawResp
 }
+
+func (gen *myGenerator) asyncCall() {
+	gen.tickets.Take()
+	go func() {
+		defer func() {
+			if p := recover(); p != nil {
+				err, ok := interface{}(p).(error)
+				var errMsg string
+				if ok {
+					errMsg = fmt.Sprintf("Async call panic! (eror: %s)", err)
+				} else {
+					errMsg = fmt.Sprintf("Async call panic!(clue: %#v)", p)
+				}
+				logger.Errorln(errMsg)
+				result := &lib.CallResult{
+					ID:   -1,
+					Code: lib.RET_CODE_FATAL_CALL,
+					Msg:  errMsg,
+				}
+				gen.sendResult(result)
+			}
+			gen.tickets.Return()
+		}()
+		rawReq := gen.caller.BuildReq()
+		var callStatus uint32
+		timer := time.AfterFunc(gen.timeoutNS, func() {
+			if !atomic.CompareAndSwapUint32(&callStatus, 0, 2) {
+				return
+			}
+			result := &lib.CallResult{
+				ID:     rawReq.ID,
+				Req:    rawReq,
+				Code:   lib.RET_CODE_WARNING_CALL_TIMEOUT,
+				Msg:    fmt.Sprintf("Timeout! (expected: < %v)", gen.timeoutNS),
+				Elapse: gen.timeoutNS,
+			}
+			gen.sendResult(result)
+		})
+		rawResp := gen.callOne(&rawReq)
+		if !atomic.CompareAndSwapUint32(&callStatus, 0, 1) {
+			return
+		}
+		timer.Stop()
+		var result *lib.CallResult
+		if rawResp.Err != nil {
+			result = &lib.CallResult{
+				ID:     rawResp.ID,
+				Req:    rawReq,
+				Code:   lib.RET_CODE_ERROR_CALL,
+				Msg:    rawResp.Err.Error(),
+				Elapse: rawResp.Elapse,
+			}
+		} else {
+			result = gen.caller.CheckResp(rawReq, *rawResp)
+			result.Elapse = rawResp.Elapse
+		}
+		gen.sendResult(result)
+	}()
+}
+
+func (gen *myGenerator) sendResult(result *lib.CallResult) bool {
+	if atomic.LoadUint32(&gen.status) != lib.STATUS_STARTED {
+		gen.printIgnoredResult(result, "stoppped load generator")
+		return false
+	}
+	select {
+	case gen.resultCh <- result:
+		return true
+	default:
+		gen.printIgnoredResult(result, "full result channel")
+		return false
+	}
+}
+
+func (gen *myGenerator) printIgnoredResult(result *lib.CallResult, cause string) {
+	resultMsg := fmt.Sprintf(
+		"ID=%d, Code=%d, Msg=%s, Elapse=%v",
+		result.ID, result.Code, result.Msg, result.Elapse)
+	logger.Warnf("Ignored result: %s. (cause: %s)\n", resultMsg, cause)
+
+}
+
+// prepareStop 用于为停止载荷发生器做准备。
+func (gen *myGenerator) prepareToStop(ctxError error) {
+	logger.Infof("Prepare to stop load generator (cause: %s)...", ctxError)
+	atomic.CompareAndSwapUint32(
+		&gen.status, lib.STATUS_STARTED, lib.STATUS_STOPPING)
+	logger.Infof("Closing result channel...")
+	close(gen.resultCh)
+	atomic.StoreUint32(&gen.status, lib.STATUS_STOPPED)
+}
+
+// genLoad 会产生载荷并向承受方发送。
+func (gen *myGenerator) genLoad(throttle <-chan time.Time) {
+	for {
+		select {
+		case <-gen.ctx.Done():
+			gen.prepareToStop(gen.ctx.Err())
+			return
+		default:
+		}
+		gen.asyncCall()
+		if gen.lps > 0 {
+			select {
+			case <-throttle:
+			case <-gen.ctx.Done():
+				gen.prepareToStop(gen.ctx.Err())
+				return
+			}
+		}
+	}
+}
+
+func (gen *myGenerator) Start() bool {
+	logger.Infoln("Starting load generator...")
+	if !atomic.CompareAndSwapUint32(&gen.status, lib.STATUS_ORIGINAL, lib.STATUS_STARTING) {
+		if !atomic.CompareAndSwapUint32(&gen.status, lib.STATUS_STOPPED, lib.STATUS_STOPPING) {
+			return false
+		}
+	}
+
+	var throttle <-chan time.Time
+	if gen.lps > 0 {
+		interval := time.Duration(1e9 / gen.lps)
+		logger.Infof("Setting throttle (%v)...", interval)
+		throttle = time.Tick(interval)
+	}
+	gen.ctx, gen.cancelFunc = context.WithTimeout(
+		context.Background(), gen.durationNS)
+	gen.callCount = 0
+
+	atomic.StoreUint32(&gen.status, lib.STATUS_STARTED)
+
+	go func() {
+		logger.Infoln("Generating loads...")
+		gen.genLoad(throttle)
+		logger.Infof("Stopped. (call count %d)", gen.callCount)
+	}()
+
+	return true
+
+}
+
+func (gen *myGenerator) Stop() bool {
+	if !atomic.CompareAndSwapUint32(&gen.status, lib.STATUS_STARTED, lib.STATUS_STOPPING) {
+		return false
+	}
+	gen.cancelFunc()
+	for {
+		if atomic.LoadUint32(&gen.status) == lib.STATUS_STOPPED {
+			break
+		}
+		time.Sleep(time.Microsecond)
+	}
+
+	return true
+}
+
+func (gen *myGenerator) Status() uint32 {
+	return atomic.LoadUint32(&gen.status)
+}
+
+func (gen *myGenerator) CallCount() int64 {
+	return atomic.LoadInt64(&gen.callCount)
+}
