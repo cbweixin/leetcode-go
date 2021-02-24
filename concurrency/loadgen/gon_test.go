0815ab1b8e9b31c7b7da8050cfabe7b044bc2c9c
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2021-02-23 22:19:48 -0800

    add new file

--- concurrency/loadgen/gon_test.go
+++ concurrency/loadgen/gon_test.go
@@ -1,8 +1,10 @@
 package loadgen
 
 import (
+	"./lib"
 	helper "./testhelper"
 	"testing"
+	"time"
 )
 
 var printDetail = false
@@ -13,5 +15,28 @@ func TestStart(t *testing.T) {
 
 	serverAddr := "127.0.0.1:8080"
 	t.Logf("Startup TCP server(%s)...\n", serverAddr)
+	err := server.Listen(serverAddr)
+	if err != nil {
+		t.Fatalf("TCP server startup failing! (addr=%s)", serverAddr)
+		t.FailNow()
+	}
 
+	pset := ParamSet{
+		Caller:     helper.NewTCPComm(serverAddr),
+		TimeoutNS:  50 * time.Millisecond,
+		LPS:        uint32(1000),
+		DurationNS: 10 * time.Second,
+		ResultCh:   make(chan *lib.CallResult, 50),
+	}
+	t.Logf("Initialize load generator (timeoutNS=%v, lps=%d, durationNS=%v)...",
+		pset.TimeoutNS, pset.LPS, pset.DurationNS)
+	gen, err := NewGenerator(pset)
+	if err != nil {
+		t.Fatalf("Load generator initialization failing: %s\n",
+			err)
+		t.FailNow()
+	}
+
+	t.Log("start load generator...")
+	gen.Start()
 }
