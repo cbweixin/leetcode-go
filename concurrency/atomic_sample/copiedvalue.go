858fa42f454871364be9109a3e140d932abfdf87
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2021-02-24 21:34:01 -0800

    add some new files

---
+++ concurrency/atomic_sample/copiedvalue.go
@@ -0,0 +1,17 @@
+package atomic_sample
+
+import (
+	"fmt"
+	"sync/atomic"
+)
+
+func StoreValue() {
+	var countVal atomic.Value
+	countVal.Store([]int{1, 3, 5, 7})
+	anotherStore(countVal)
+	fmt.Printf("The count value: %+v \n", countVal.Load())
+}
+
+func anotherStore(countVal atomic.Value) {
+	countVal.Store([]int{2, 4, 6, 8})
+}
