a33346d44780cbdcbb33b2461885f7c8cfd18b1c
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2021-02-28 14:07:11 -0800

    add new file

---
+++ concurrency/pool/pool_sample.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"runtime/debug"
+	"sync"
+	"sync/atomic"
+)
+
+func main() {
+	// 禁用GC，并保证在main函数执行结束前恢复GC。
+	defer debug.SetGCPercent(debug.SetGCPercent(-1))
+	var count int32
+	newFunc := func() interface{} {
+		return atomic.AddInt32(&count, 1)
+	}
+
+	pool := sync.Pool{New: newFunc}
+
+	v1 := pool.Get()
+	fmt.Printf("Value 1: %v\n", v1)
+
+	pool.Put(10)
+	pool.Put(11)
+	pool.Put(12)
+
+	v2 := pool.Get()
+	fmt.Printf("value 2 : %v\n", v2)
+
+	debug.SetGCPercent(100)
+	runtime.GC()
+	v3 := pool.Get()
+	fmt.Printf("value 3 : %v\n", v3)
+	pool.New = nil
+	v4 := pool.Get()
+	fmt.Printf("value 4 : %v\n", v4)
+
+}
