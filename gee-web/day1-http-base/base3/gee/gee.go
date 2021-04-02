2e49c5a6278c8d8593cd44990ff2e95cf9fd3987
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2021-04-02 07:31:19 -0700

    add some changes

--- gee-web/day1-http-base/base3/gee/gee.go
+++ gee-web/day1-http-base/base3/gee/gee.go
@@ -1,6 +1,7 @@
 package gee
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -19,4 +20,28 @@ func (engine *Engine) addRoute(method string, pattern string, handler HandlerFun
 	key := method + "-" + pattern
 	log.Printf("Route %4s - %s", method, pattern)
 	engine.router[key] = handler
+}
+
+// GET defines the method to add GET request
+func (engine *Engine) GET(pattern string, handler HandlerFunc) {
+	engine.addRoute("GET", pattern, handler)
+}
+
+// POST defines the method to add POST request
+func (engine *Engine) POST(pattern string, handler HandlerFunc) {
+	engine.addRoute("POST", pattern, handler)
+}
+
+// Run defines the method to start a http server
+func (engine *Engine) Run(addr string) (err error) {
+	return http.ListenAndServe(addr, engine)
+}
+
+func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	key := req.Method + "-" + req.URL.Path
+	if handler, ok := engine.router[key]; ok {
+		handler(w, req)
+	} else {
+		fmt.Fprintf(w, "404 NOT FOUND: %s\n", req.URL)
+	}
 }
