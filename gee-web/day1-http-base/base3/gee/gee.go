96fb949587738ff8b80ab1f99c0349108d831594
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2021-04-02 07:34:47 -0700

    add some comments

--- gee-web/day1-http-base/base3/gee/gee.go
+++ gee-web/day1-http-base/base3/gee/gee.go
@@ -6,6 +6,10 @@ import (
 	"net/http"
 )
 
+/**
+首先定义了类型HandlerFunc，这是提供给框架用户的，用来定义路由映射的处理方法。我们在Engine中，添加了一张路由映射表router，key 由请求方法和
+静态路由地址构成，例如GET-/、GET-/hello、POST-/hello，这样针对相同的路由，如果请求方法不同,可以映射不同的处理方法(Handler)，value 是用户映射的处理方法。
+*/
 type HandlerFunc func(http.ResponseWriter, *http.Request)
 
 type Engine struct {
@@ -23,6 +27,7 @@ func (engine *Engine) addRoute(method string, pattern string, handler HandlerFun
 }
 
 // GET defines the method to add GET request
+// 当用户调用(*Engine).GET()方法时，会将路由和处理方法注册到映射表 router 中，(*Engine).Run()方法，是 ListenAndServe 的包装。
 func (engine *Engine) GET(pattern string, handler HandlerFunc) {
 	engine.addRoute("GET", pattern, handler)
 }
@@ -33,10 +38,13 @@ func (engine *Engine) POST(pattern string, handler HandlerFunc) {
 }
 
 // Run defines the method to start a http server
+
 func (engine *Engine) Run(addr string) (err error) {
 	return http.ListenAndServe(addr, engine)
 }
 
+// Engine实现的 ServeHTTP 方法的作用就是，解析请求的路径，查找路由映射表，如果查到，就执行注册的处理方法。如果查不到，就返回
+//404 NOT FOUND 。
 func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	key := req.Method + "-" + req.URL.Path
 	if handler, ok := engine.router[key]; ok {
--- gee-web/day1-http-base/base3/go.mod
+++ gee-web/day1-http-base/base3/go.mod
@@ -2,7 +2,6 @@ module example
 
 go 1.16
 
-
 require gee v0.0.0
 
 replace gee => ./gee
