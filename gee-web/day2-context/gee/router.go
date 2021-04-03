b48e22bc8f0f981c066e95841736ae3904ed9351
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2021-04-03 07:09:37 -0700

    add new changes

---
+++ gee-web/day2-context/gee/router.go
@@ -0,0 +1,25 @@
+package gee
+
+import "net/http"
+
+type router struct {
+	handlers map[string]HandlerFunc
+}
+
+func newRouter() *router {
+	return &router{handlers: make(map[string]HandlerFunc)}
+}
+
+func (r *router) addRoute(method string, patten string, handler HandlerFunc) {
+	key := method + "-" + patten
+	r.handlers[key] = handler
+}
+
+func (r *router) handle(c *Context) {
+	key := c.Method + "-" + c.Path
+	if handler, ok := r.handlers[key]; ok {
+		handler(c)
+	} else {
+		c.String(http.StatusNotFound, "404 Not Found: %s\n", c.Path)
+	}
+}
