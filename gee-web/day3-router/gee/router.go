3c124040353c2e752bb04f2c55a411680906137b
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2021-04-07 21:55:12 -0700

    add new files

---
+++ gee-web/day3-router/gee/router.go
@@ -0,0 +1,14 @@
+package gee
+
+type router struct {
+	// why node need to be pointer?
+	roots    map[string]*node
+	handlers map[string]HandlerFunc
+}
+
+func newRouter() *router {
+	return &router{
+		roots:    make(map[string]*node),
+		handlers: make(map[string]HandlerFunc),
+	}
+}
