2e640ee010a917a620fc69741640014d6813ce89
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2021-04-12 06:43:58 -0700

    add some new files

---
+++ gee-web/day3-router/go.mod
@@ -0,0 +1,6 @@
+module "example"
+
+require (
+	gee v0.0.0
+)
+replace gee => ./gee
---
+++ gee-web/day3-router/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"gee"
+	"net/http"
+)
+
+// ❯ curl http://localhost:9999/
+//<h1>Hello Gee</h1>%
+
+// ❯ curl http://localhost:9999/
+//<h1>Hello Gee</h1>%
+
+// ❯ curl http://localhost:9999/
+//<h1>Hello Gee</h1>%
+
+// ❯ curl http://localhost:9999/hello/tom/and/jerry
+//404 NOT FOUND: /hello/tom/and/jerry
+
+// ❯ curl http://localhost:9999/hello/tom/and/jerry
+//404 NOT FOUND: /hello/tom/and/jerry
+
+func main() {
+	r := gee.New()
+	r.GET("/", func(c *gee.Context) {
+		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+	})
+
+	r.GET("/hello", func(c *gee.Context) {
+		// expect /hello?name=geektutu
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+	})
+
+	r.GET("/hello/:name", func(c *gee.Context) {
+		// expect /hello/geektutu
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+	})
+
+	r.GET("/assets/*filepath", func(c *gee.Context) {
+		c.JSON(http.StatusOK, gee.H{"filepath": c.Param("filepath")})
+	})
+
+	r.Run(":9999")
+}
