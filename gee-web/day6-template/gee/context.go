b98ebc913f4ddeb5e7f3a052506ce8f3104a5759
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2021-04-18 22:37:59 -0700

    add new file

---
+++ gee-web/day6-template/gee/context.go
@@ -0,0 +1,32 @@
+package gee
+
+import "net/http"
+
+type H map[string]interface{}
+
+type Context struct {
+	// origin objects
+	Writer http.ResponseWriter
+	Req    *http.Request
+	// request info
+	Path   string
+	Method string
+	Params map[string]string
+	// response info
+	StatusCode int
+	// middleware
+	handlers []HandlerFunc
+	index    int
+	// engine pointer
+	engine *Engine
+}
+
+func newContext(w http.ResponseWriter, req *http.Request) *Context {
+	return &Context{
+		Path:   req.URL.Path,
+		Method: req.Method,
+		Req:    req,
+		Writer: w,
+		index:  -1,
+	}
+}
