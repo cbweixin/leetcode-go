fae0e0d941333e6d9c97eb963db69661151d3e55
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2021-04-19 06:53:20 -0700

    add new changes

--- gee-web/day6-template/gee/context.go
+++ gee-web/day6-template/gee/context.go
@@ -30,3 +30,34 @@ func newContext(w http.ResponseWriter, req *http.Request) *Context {
 		index:  -1,
 	}
 }
+
+func (c *Context) Next() {
+	c.index++
+	s := len(c.handlers)
+	for ; c.index < s; c.index++ {
+		c.handlers[c.index](c)
+	}
+}
+
+func (c *Context) Fail(code int, err string) {
+	c.index = len(c.handlers)
+	c.JSON(code, H{"message": err})
+}
+
+func (c *Context) Param(key string) string {
+	value, _ := c.Params[key]
+	return value
+}
+
+func (c *Context) PostForm(key string) string {
+	return c.Req.FormValue(key)
+}
+
+func (c *Context) Query(key string) string {
+	return c.Req.URL.Query().Get(key)
+}
+
+func (c *Context) Status(code int) {
+	c.StatusCode = code
+	c.Writer.WriteHeader(code)
+}
