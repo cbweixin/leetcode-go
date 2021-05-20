b24136f4ee993eb4432a0b4d68a04d60fc571e15
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2021-05-19 21:25:16 -0700

    9:24 PM	info 				Wrong Answer: 				input:[1,2] 				4 				Output:3 				Expected:-1 				stdout:

---
+++ leetcode/editor/cn/shortest-subarray-with-sum-at-least-k-862.go
@@ -0,0 +1,75 @@
+package main
+
+//返回 A 的最短的非空连续子数组的长度，该子数组的和至少为 K 。
+//
+// 如果没有和至少为 K 的非空子数组，返回 -1 。
+//
+//
+//
+//
+//
+//
+// 示例 1：
+//
+// 输入：A = [1], K = 1
+//输出：1
+//
+//
+// 示例 2：
+//
+// 输入：A = [1,2], K = 4
+//输出：-1
+//
+//
+// 示例 3：
+//
+// 输入：A = [2,-1,2], K = 3
+//输出：3
+//
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= A.length <= 50000
+// -10 ^ 5 <= A[i] <= 10 ^ 5
+// 1 <= K <= 10 ^ 9
+//
+// Related Topics 队列 二分查找
+// 👍 278 👎 0
+
+// 2021-05-18 21:29:26
+//leetcode submit region begin(Prohibit modification and deletion)
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+func shortestSubarray(nums []int, k int) int {
+	l := len(nums)
+	preSum := make([]int, l+1)
+	for i := 0; i < l; i++ {
+		preSum[i+1] = preSum[i] + nums[i]
+	}
+	// review how to simulate a stack
+	queue := make([]int, 0)
+	res := l + 1
+	for i, v := range preSum {
+		for len(queue) > 0 && v <= preSum[queue[len(queue)-1]] {
+			queue = queue[:len(queue)-1]
+		}
+		for len(queue) > 0 && v-preSum[queue[0]] >= k {
+			res = min(res, i-queue[0])
+			queue = queue[1:]
+		}
+		queue = append(queue, i)
+	}
+
+	return res
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
