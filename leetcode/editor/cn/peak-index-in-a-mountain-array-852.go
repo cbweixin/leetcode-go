e553f08786ba39f0403c20bcc3d58be67986d993
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2021-06-15 22:34:04 -0700

    行结果： 解答错误 显示详情 添加备注
    输入：
    [0,1,0]
    输出：
    2
    预期结果：
    1

--- leetcode/editor/cn/peak-index-in-a-mountain-array-852.go
+++ leetcode/editor/cn/peak-index-in-a-mountain-array-852.go
@@ -72,21 +72,19 @@ package main
 func peakIndexInMountainArray(arr []int) int {
 	l, r := 0, len(arr)-1
 
-	for l <= r {
+	for l < r {
 		mid := (l + r) >> 1
-		if arr[mid] > arr[mid-1] && arr[mid] > arr[mid+1] {
-			return mid
-		}
 		if arr[mid] > arr[mid-1] {
 			l = mid + 1
 		} else if arr[mid] > arr[mid+1] {
-			r = mid - 1
+			r = mid
 		}
 	}
-	if l == r {
-		return l
-	}
-	return -1
+	return l
 }
 
 //leetcode submit region end(Prohibit modification and deletion)
+func main() {
+	arr := []int{3, 4, 5, 1, 0}
+	println(peakIndexInMountainArray(arr))
+}
