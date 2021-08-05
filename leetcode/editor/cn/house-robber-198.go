4e66c2cc85472e7224be9e48f35469c07b0c41f5
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2021-08-05 14:54:56 -0700

    Success: Runtime:0 ms, faster than 100.00% of Go online submissions. Memory Usage:2 MB, less than 83.88% of Go online submissions.

---
+++ leetcode/editor/cn/house-robber-198.go
@@ -0,0 +1,63 @@
+package main
+
+//你是一个专业的小偷，计划偷窃沿街的房屋。每间房内都藏有一定的现金，影响你偷窃的唯一制约因素就是相邻的房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上
+//被小偷闯入，系统会自动报警。
+//
+// 给定一个代表每个房屋存放金额的非负整数数组，计算你 不触动警报装置的情况下 ，一夜之内能够偷窃到的最高金额。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：[1,2,3,1]
+//输出：4
+//解释：偷窃 1 号房屋 (金额 = 1) ，然后偷窃 3 号房屋 (金额 = 3)。
+//     偷窃到的最高金额 = 1 + 3 = 4 。
+//
+// 示例 2：
+//
+//
+//输入：[2,7,9,3,1]
+//输出：12
+//解释：偷窃 1 号房屋 (金额 = 2), 偷窃 3 号房屋 (金额 = 9)，接着偷窃 5 号房屋 (金额 = 1)。
+//     偷窃到的最高金额 = 2 + 9 + 1 = 12 。
+//
+//
+//
+//
+// 提示：
+//
+//
+// 1 <= nums.length <= 100
+// 0 <= nums[i] <= 400
+//
+// Related Topics 数组 动态规划
+// 👍 1584 👎 0
+
+// 2021-08-05 14:47:59
+//leetcode submit region begin(Prohibit modification and deletion)
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func rob(nums []int) int {
+	last, now := 0, 0
+	for _, v := range nums {
+		temp := last
+		last = now
+		now = max(temp+v, now)
+	}
+	return now
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
+func main() {
+	houses := []int{1, 2, 3, 1}
+	println(rob(houses))
+
+}
