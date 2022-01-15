894622886df9b129fb03c30e9e4b8d66ec3a48a8
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2022-01-15 09:58:15 -0800

    Success: Runtime:0 ms, faster than 100.00% of Go online submissions. Memory Usage:2.3 MB, less than 89.83% of Go online submissions.

--- leetcode/editor/cn/minimum-cost-for-tickets-983.go
+++ leetcode/editor/cn/minimum-cost-for-tickets-983.go
@@ -76,13 +76,20 @@ func mincostTickets(days []int, costs []int) int {
 
 		return x
 	}
-	find_max := func(a, b, c int) int {
-		return max(a, max(b, c))
+
+	min := func(x, y int) int {
+		if x < y {
+			return x
+		}
+		return y
+	}
+	findMin := func(a, b, c int) int {
+		return min(a, min(b, c))
 	}
 
-	for i := 0; i < n+1; i++ {
+	for i := 1; i < n+1; i++ {
 		if find_item(i) {
-			dp[i] = find_max(dp[i-1]+costs[0], dp[max(i-7, 0)]+costs[1], dp[max(i-30, 0)]+costs[2])
+			dp[i] = findMin(dp[i-1]+costs[0], dp[max(i-7, 0)]+costs[1], dp[max(i-30, 0)]+costs[2])
 		} else {
 			dp[i] = dp[i-1]
 		}
