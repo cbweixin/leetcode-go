7edc76cfc964618373cacb9a0bd5319a2cbcefb1
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2022-04-09 17:45:47 -0700

    Runtime: 4 ms, faster than 89.47% of Go online submissions for Fixed Point. Memory Usage: 4.3 MB, less than 82.71% of Go online submissions for Fixed Point.

---
+++ leetcode/editor/cn/fix-point-1064.go
@@ -0,0 +1,30 @@
+package main
+
+func fixedPoint1(arr []int) int {
+	for i, v := range arr {
+		if v == i {
+			return i
+		}
+	}
+	return -1
+}
+
+func fixedPoint(arr []int) int {
+	var l int = 0
+	var r int = len(arr) - 1
+
+	for l < r {
+		var mid int = (l + r) >> 1
+		if arr[mid]-mid < 0 {
+			l = mid + 1
+		} else {
+			r = mid
+		}
+	}
+
+	if arr[l] == l {
+		return l
+	}
+	return -1
+
+}
