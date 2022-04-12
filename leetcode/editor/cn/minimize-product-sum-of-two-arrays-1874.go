671c5f83a9be5c9ecb85301a746410934aaf794a
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2022-04-11 19:09:44 -0700

    Success Details Runtime: 181 ms, faster than 77.78% of Go online submissions for Minimize Product Sum of Two Arrays. Memory Usage: 9.8 MB, less than 75.56% of Go online submissions for Minimize Product Sum of Two Arrays.

--- leetcode/editor/cn/minimize-product-sum-of-two-arrays-1874.go
+++ leetcode/editor/cn/minimize-product-sum-of-two-arrays-1874.go
@@ -14,3 +14,31 @@ func minProductSum(nums1 []int, nums2 []int) int {
 
 	return res
 }
+
+func minProductSum1(nums1 []int, nums2 []int) int {
+	// 1 <= nums1[i], nums2[i] <= 100
+	bucket1, bucket2 := make([]int, 101), make([]int, 102)
+	l := len(nums1)
+
+	for i := 0; i < l; i++ {
+		bucket1[nums1[i]] += 1
+		bucket2[nums2[i]] += 1
+	}
+
+	left, right, res := 0, 100, 0
+	for left < 101 && right >= 0 {
+		if bucket1[left] > 0 && bucket2[right] > 0 {
+			res += left * right
+			bucket1[left]--
+			bucket2[right]--
+		} else if bucket1[left] > 0 {
+			right--
+		} else if bucket2[right] > 0 {
+			left++
+		} else {
+			left++
+			right--
+		}
+	}
+	return res
+}
