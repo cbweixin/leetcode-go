b04025f7124742ebb4388575968669692ee81e37
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2022-04-16 11:00:36 -0700

    Success Details Runtime: 46 ms, faster than 81.82% of Go online submissions for Missing Element in Sorted Array. Memory Usage: 7 MB, less than 96.10% of Go online submissions for Missing Element in Sorted Array.

--- leetcode/editor/cn/missing-element-in-sorted-array-1060.go
+++ leetcode/editor/cn/missing-element-in-sorted-array-1060.go
@@ -1,6 +1,6 @@
 package main
 
-func missingElement(nums []int, k int) int {
+func missingElement1(nums []int, k int) int {
 	res, cur, i, l := -1, nums[0]+1, 1, len(nums)
 	for k > 0 {
 		if (i < l && cur != nums[i]) || i >= l {
@@ -12,4 +12,17 @@ func missingElement(nums []int, k int) int {
 		cur++
 	}
 	return res
+}
+
+func missingElement(nums []int, k int) int {
+	l, r := 0, len(nums)-1
+	for l <= r {
+		mid := (l + r) >> 1
+		if nums[mid]-nums[0]-mid >= k {
+			r = mid - 1
+		} else {
+			l = mid + 1
+		}
+	}
+	return nums[0] + k + r
 }
