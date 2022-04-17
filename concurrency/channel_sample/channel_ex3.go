fafbe31ba88ad284637e7dc75b8e6a8a5799a3ad
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2022-04-16 20:06:09 -0700

    add some comemnts

--- concurrency/channel_sample/channel_ex3.go
+++ concurrency/channel_sample/channel_ex3.go
@@ -30,7 +30,30 @@ func Chan_With_Map() {
 		close(mapChan)
 		syncChan <- struct{}{}
 	}()
+	// what would happen if I remove one <-synChan? below is what I got
+	// The count map: map[count:1]. [sender]
+	// The count map: map[count:2]. [sender]
+	// The count map: map[count:3]. [sender]
+	// The count map: map[count:4]. [sender]
+	// The count map: map[count:5]. [sender]
+
+	// where is "Stopped. [receiver]"?
+	// we have : 	syncChan := make(chan struct{}, 2)
+	// both sender and reciever would do this : syncChan <- struct{}{}
+	// so when the main program would call <-synChan twice, the main program would blocked
+	// and wait for both reciever and sender to finish its task, if we have only one <-synChan
+	// then main program would not wait both, most likey the send do syncChan <- struct{}{}, then the main program
+	// just exit, so we will never saw "Stopped. [receiver] "
 	<-syncChan
 	<-syncChan
 
 }
+
+/**
+The count map: map[count:1]. [sender]
+The count map: map[count:2]. [sender]
+The count map: map[count:3]. [sender]
+The count map: map[count:4]. [sender]
+The count map: map[count:5]. [sender]
+Stopped. [receiver]
+*/
