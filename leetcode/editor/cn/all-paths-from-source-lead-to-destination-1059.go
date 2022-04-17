84ca3e0a21d7a68a4ecb03509c7d4878343b0f41
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2022-04-16 21:13:36 -0700

    Success Details Runtime: 40 ms, faster than 76.67% of Go online submissions for All Paths from Source Lead to Destination. Memory Usage: 7.4 MB, less than 73.33% of Go online submissions for All Paths from Source Lead to Destination. Next challenges:

--- leetcode/editor/cn/all-paths-from-source-lead-to-destination-1059.go
+++ leetcode/editor/cn/all-paths-from-source-lead-to-destination-1059.go
@@ -1,5 +1,9 @@
 package main
 
+type Adj struct {
+	ajs []int
+}
+
 func leadsToDestination(n int, edges [][]int, source int, destination int) bool {
 	colors := make([]int, n)
 	graph := make(map[int][]int)
@@ -17,6 +21,41 @@ func leadsToDestination(n int, edges [][]int, source int, destination int) bool 
 		colors[src] = 1
 		if len(graph[src]) > 0 {
 			for _, v := range graph[src] {
+				if colors[v] == 1 {
+					return false
+				}
+				if colors[v] == 0 && !dfs(v) {
+					return false
+				}
+			}
+			colors[src] = 2
+		}
+		return true
+	}
+	return dfs(source)
+}
+
+func leadsToDestination1(n int, edges [][]int, source int, destination int) bool {
+	colors := make([]int, n)
+	graph := make(map[int]*Adj)
+
+	for i := 0; i < n; i++ {
+		graph[i] = &Adj{ajs: []int{}}
+	}
+
+	for _, arr := range edges {
+		s, d := arr[0], arr[1]
+		graph[s].ajs = append(graph[s].ajs, d)
+	}
+
+	var dfs func(int) bool
+	dfs = func(src int) bool {
+		if len(graph[src].ajs) == 0 {
+			return src == destination
+		}
+		colors[src] = 1
+		if len(graph[src].ajs) > 0 {
+			for _, v := range graph[src].ajs {
 				if colors[v] == 1 {
 					return false
 				}
