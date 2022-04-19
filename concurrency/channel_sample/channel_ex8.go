5d7c443e5d37e2ebc0f70048cdaf668b304b0ac5
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2022-04-18 22:41:28 -0700

    add some comments

--- concurrency/channel_sample/channel_ex8.go
+++ concurrency/channel_sample/channel_ex8.go
@@ -5,22 +5,41 @@ import "fmt"
 func Chan_Conv() {
 	var ok bool
 	ch := make(chan int, 1)
+	// true
 	_, ok = interface{}(ch).(chan int)
 	fmt.Println("chan int => chan int:", ok)
+	// false, channel with different direction is different type
 	_, ok = interface{}(ch).(<-chan int)
 	fmt.Println("chan int => <-chan int:", ok)
+	// false
 	_, ok = interface{}(ch).(chan<- int)
 	fmt.Println("chan int => chan<- int:", ok)
 
 	sch := make(chan<- int, 1)
+	// true
 	_, ok = interface{}(sch).(chan<- int)
 	fmt.Println("chan<- int => chan<- int:", ok)
+	// false
 	_, ok = interface{}(sch).(chan int)
 	fmt.Println("chan<- int => chan int:", ok)
 
 	rch := make(<-chan int, 1)
+	// true
 	_, ok = interface{}(rch).(<-chan int)
 	fmt.Println("<-chan int => <-chan int:", ok)
+	// false
 	_, ok = interface{}(rch).(chan int)
 	fmt.Println("<-chan int => chan int:", ok)
 }
+
+/**
+chan int => chan int: true
+chan int => <-chan int: false
+chan int => chan<- int: false
+chan<- int => chan<- int: true
+chan<- int => chan int: false
+<-chan int => <-chan int: true
+<-chan int => chan int: false
+
+Program exited.
+*/
