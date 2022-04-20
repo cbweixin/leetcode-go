177a63a716bd246e5ea4494539a73461f84a6315
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2022-04-19 23:51:53 -0700

    add some comments

--- concurrency/channel_sample/channel_ex11.go
+++ concurrency/channel_sample/channel_ex11.go
@@ -5,6 +5,8 @@ import "fmt"
 func Select_test2() {
 	chanCap := 5
 	intChan := make(chan int, chanCap)
+	// each time , it would have a different output
+	// so when mutiple case is satisifed, go would use some pesudo-random algo to choose a branch
 	for i := 0; i < chanCap; i++ {
 		select {
 		case intChan <- 1:
@@ -16,3 +18,23 @@ func Select_test2() {
 		fmt.Printf("%d\n", <-intChan)
 	}
 }
+
+/**
+1
+1
+3
+1
+1
+or
+3
+1
+1
+3
+1
+or
+2
+3
+3
+1
+3
+*/
