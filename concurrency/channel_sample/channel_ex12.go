128b9841a4a76d15a707a3e90653259a37f8c540
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2022-04-22 10:53:43 -0700

    add some comments

--- concurrency/channel_sample/channel_ex12.go
+++ concurrency/channel_sample/channel_ex12.go
@@ -10,14 +10,14 @@ func Select_test3() {
 	close(intChan)
 	syncChan := make(chan struct{}, 1)
 	go func() {
-	Loop:
+	Loop: // notice the usage of label
 		for {
 			select {
 			case e, ok := <-intChan:
 				if !ok {
 					fmt.Println("End.")
 					// be aware of the tag LOOP , without it, you just break `select` instead of `for` loop
-					break Loop
+					break Loop // how to break a label
 				}
 				fmt.Printf("Received: %v\n", e)
 			}
@@ -26,3 +26,17 @@ func Select_test3() {
 	}()
 	<-syncChan
 }
+
+/**
+Received: 0
+Received: 1
+Received: 2
+Received: 3
+Received: 4
+Received: 5
+Received: 6
+Received: 7
+Received: 8
+Received: 9
+End.
+*/
