41fd5003f7ebe4097b8d94da3f90adf14a701032
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2022-04-22 23:15:25 -0700

    add some comments

--- concurrency/channel_sample/channel_ex13.go
+++ concurrency/channel_sample/channel_ex13.go
@@ -44,3 +44,24 @@ Loop:
 	}
 	fmt.Println("End.")
 }
+
+/**
+Sent: 1
+Received: 1
+Received: 2 [interval: 2 s]
+Sent: 2 [interval: 2 s]
+Received: 3 [interval: 2 s]
+Sent: 3 [interval: 2 s]
+Received: 4 [interval: 2 s]
+Sent: 4 [interval: 2 s]
+Received: 5 [interval: 2 s]
+Sent: 5 [interval: 2 s]
+End.
+happens-before 原则
+
+向此类通道发送元素值的操作会被阻塞，直到至少有一个针对该 通道的接收操作进行为止。该接收操作会先得到元素值的副本，
+然后在唤醒发送方所在的goroutine之后返回。也就是说，这时的 接收操作会在对应的发送操作完成之前完成。
+
+从此类通道接收元素值的操作会被阻塞，直到至少有一个针对该 通道的发送操作进行为止。该发送操作会直接把元素值复制给接
+收方，然后在唤醒接收方所在的goroutine之后返回。也就是说， 这时的发送操作会在对应的接收操作完成之前完成。
+*/
