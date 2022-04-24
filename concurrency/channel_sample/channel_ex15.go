6ea680c8c811021c4c583e1fc1fdf6a06354b233
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2022-04-23 22:13:57 -0700

    add some comment

--- concurrency/channel_sample/channel_ex15.go
+++ concurrency/channel_sample/channel_ex15.go
@@ -14,7 +14,13 @@ func Timer_test2() {
 	select {
 	case e := <-intChan:
 		fmt.Printf("Received: %v\n", e)
+	// when case below get the value because timer timout, the select would terminated
+	// then case above has no oppturnity to execute, by this way you implement the timemout mechanism.
 	case <-time.NewTimer(time.Millisecond * 500).C:
 		fmt.Println("Timeout!")
 	}
 }
+
+/**
+Timeout!
+*/
