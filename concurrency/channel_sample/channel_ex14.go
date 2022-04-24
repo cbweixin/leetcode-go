779130f6389a143cad497c9977250c63d472f799
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2022-04-23 21:43:58 -0700

    执行结果： 通过 显示详情 添加备注
    执行用时：
    4 ms
    , 在所有 Go 提交中击败了
    83.12%
    的用户
    内存消耗：
    4.5 MB
    , 在所有 Go 提交中击败了
    5.19%
    的用户
    通过测试用例：
    171 / 171

--- concurrency/channel_sample/channel_ex14.go
+++ concurrency/channel_sample/channel_ex14.go
@@ -12,3 +12,9 @@ func Timer_test() {
 	fmt.Printf("Expiration time: %v.\n", expirationTime)
 	fmt.Printf("Stop timer: %v.\n", timer.Stop())
 }
+
+/**
+Present time: 2009-11-10 23:00:00 +0000 UTC m=+0.000000001.
+Expiration time: 2009-11-10 23:00:02 +0000 UTC m=+2.000000001.
+Stop timer: false.
+*/
