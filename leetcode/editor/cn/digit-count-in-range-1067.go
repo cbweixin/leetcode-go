ffc77479c09ac17535f1b01d644e6887476cc288
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2022-04-30 15:24:24 -0700

    Success Details Runtime: 0 ms, faster than 100.00% of Go online submissions for Digit Count in Range. Memory Usage: 2 MB, less than 100.00% of Go online submissions for Digit Count in Range.

--- leetcode/editor/cn/digit-count-in-range-1067.go
+++ leetcode/editor/cn/digit-count-in-range-1067.go
@@ -5,7 +5,7 @@ func digitsCount(d int, low int, high int) int {
 	countDigit := func(n int) int {
 		k, res := 1, 0
 		for k = 1; k <= n; k *= 10 {
-			r, m := n%10, n/10
+			r, m := n%k, n/k
 			e, xyz := m%10, m/10
 			res += xyz * k
 			if d == 0 {
