6947e25c6946109a4af7133874cccf621a715a35
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2022-04-30 15:23:47 -0700

    Wrong Answer Runtime: 1 ms Your input 1 1 13 Output 8 Expected 6

---
+++ leetcode/editor/cn/digit-count-in-range-1067.go
@@ -0,0 +1,23 @@
+package main
+
+func digitsCount(d int, low int, high int) int {
+
+	countDigit := func(n int) int {
+		k, res := 1, 0
+		for k = 1; k <= n; k *= 10 {
+			r, m := n%10, n/10
+			e, xyz := m%10, m/10
+			res += xyz * k
+			if d == 0 {
+				res -= k
+			}
+			if e > d {
+				res += k
+			} else if e == d {
+				res += 1 + r
+			}
+		}
+		return res
+	}
+	return countDigit(high) - countDigit(low-1)
+}
