29af697fb98b0bd6e6ada702e6c3cb83ee5b1626
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2022-06-10 15:29:56 -0700

    add some new files

--- pointer_ex.go
+++ pointer_ex.go
@@ -11,6 +11,18 @@ func main() {
 	n := (*[4]byte)(p)
 
 	for i := 0; i < len(n); i++ {
-		fmt.Printf("%X", n[i])
+		fmt.Printf("%X\n", n[i])
 	}
+
+	d := struct {
+		s string
+		x int
+	}{"abc", 100}
+
+	p1 := uintptr(unsafe.Pointer(&d))
+	p1 += unsafe.Offsetof(d.x)
+	p2 := unsafe.Pointer(p1)
+	px := (*int)(p2)
+	*px = 200
+	fmt.Printf("%#v\n", d)
 }
