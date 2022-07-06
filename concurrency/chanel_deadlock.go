1b7ada7146823d1757506a9af004a6b17bac1abb
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2022-07-05 23:09:26 -0700

    add some file

---
+++ concurrency/chanel_deadlock.go
@@ -0,0 +1,29 @@
+package main
+
+// 只有生产者，没有消费者
+func f1() {
+	ch := make(chan int)
+	ch <- 1
+}
+
+// 只有消费者，没有生产者
+func f2() {
+	ch := make(chan int)
+	<-ch
+}
+
+func f3() {
+	ch := make(chan int)
+	// producer blocked the routine, consumer has no opportunity to run
+	ch <- 1
+	// this line not able to reach
+	<-ch
+
+}
+
+func main() {
+	go f2()
+	go f1()
+	f3()
+	print("hello")
+}
