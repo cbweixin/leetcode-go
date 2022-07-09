129d670c9cb3aba99c1bd985adcb13b807675155
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2022-07-09 13:26:33 -0700

    add more changes

---
+++ concurrency/channel_as_future_promise.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+// Return receive-only channels as results
+func longTimeRequest() <-chan int32 {
+	r := make(chan int32)
+
+	go func() {
+		time.Sleep(3 * time.Second)
+		r <- rand.Int31n(100)
+	}()
+
+	return r
+}
+
+// Pass send-only channels as arguments
+func longTimeRequest2(r chan<- int32) {
+	time.Sleep(3 * time.Second)
+	r <- rand.Int31n(100)
+
+}
+func sumSqures(a, b int32) int32 {
+	return a*a + b*b
+}
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	a, b := longTimeRequest(), longTimeRequest()
+	fmt.Println(sumSqures(<-a, <-b))
+}
