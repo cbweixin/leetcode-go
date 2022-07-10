19f6184375f85a4cc213a7aaf1e4f7611cef99c5
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2022-07-10 16:26:53 -0700

    add new examples

--- concurrency/channel_for_notification.go
+++ concurrency/channel_for_notification.go
@@ -164,6 +164,44 @@ func runBar() {
 
 }
 
+// In the above example, although there will be most ten customers are drinking at any given time, there may be more
+// than ten customers are served at the bar at the same time. Some customers are waiting for free seats. Although each
+// customer goroutine consumes much fewer resources than a system thread, the total resources consumed by a large number
+// of goroutines are not negligible. So it is best to create a customer goroutine only if there is an available seat.
+
+func (bar Bar) serveCustomerAtSeat(c int, seat Seat) {
+	log.Print("++ customer #", c, " drinks at seat #", seat)
+	time.Sleep(time.Second * time.Duration(2+rand.Intn(6)))
+
+	log.Print("-- customer#", c, " frees seat#", seat)
+	bar <- seat // free seat and leave the bar
+}
+
+func runBar2() {
+	rand.Seed(time.Now().UnixNano())
+
+	bar24x7 := make(Bar, 10) // the bar has 10 seats
+	// place seat in bar
+	for seatId := 0; seatId < cap(bar24x7); seatId++ {
+		// none of the sends will block
+		bar24x7 <- Seat(seatId)
+	}
+
+	for customerId := 0; ; customerId++ {
+		time.Sleep(time.Second)
+		seat := <-bar24x7
+		// critical only when seat is available, then create goroutine
+		go bar24x7.serveCustomerAtSeat(customerId, seat)
+	}
+
+	// The last for loop in the main function is to avoid the program exiting. There is a better way, which will be
+	// introduced below, to do the job.
+	for { // sleeping != blocking
+		time.Sleep(time.Second)
+	}
+
+}
+
 func main() {
 	// 1-to-1 notification by sending a value to a channel
 
@@ -270,5 +308,5 @@ func main() {
 	mutexEx1()
 	mutexEx2()
 
-	runBar()
+	runBar2()
 }
