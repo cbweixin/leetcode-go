4b0362b590443e9fc3128c0530c99da341e28f41
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2022-07-17 14:45:31 -0700

    reformat

--- concurrency/ratelimiter/rate_limiter_ex.go
+++ concurrency/ratelimiter/rate_limiter_ex.go
@@ -2,22 +2,25 @@ package ratelimiter
 
 import (
 	"fmt"
-	"golang.org/x/time/rate"
 	"net/http"
 	"time"
+
+	"golang.org/x/time/rate"
 )
 
 func Serve() {
 	// 1 / (1 * time.Millisecond).Seconds() = 1000
 	r := rate.Every(1 * time.Millisecond)
 	limiter := rate.NewLimiter(r, 10)
-	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		if limiter.Allow() {
-			fmt.Printf("请求成功，当前时间：%s\n", time.Now().Format("2022-01-02 15:04:05"))
-		} else {
-			fmt.Printf("请求成功，但是被限流了。。。\n")
-		}
-	})
+	http.HandleFunc(
+		"/", func(writer http.ResponseWriter, request *http.Request) {
+			if limiter.Allow() {
+				fmt.Printf("请求成功，当前时间：%s\n", time.Now().Format("2022-01-02 15:04:05"))
+			} else {
+				fmt.Printf("请求成功，但是被限流了。。。\n")
+			}
+		},
+	)
 
 	_ = http.ListenAndServe(":8081", nil)
 }
