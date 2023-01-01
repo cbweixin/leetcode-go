b5b122c24b8716f804b4e3c1623b3623b0c0d7c8
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2022-12-31 18:08:27 -0800

    Compile Error: 	Line 127: Char 6: main redeclared in this block (solution.go) Line 113: Char 6: previous declaration (solution.go) 	Your input:[1] 			[1,1] 			[1,2,3] 	stdout:

--- leetcode/editor/cn/game-of-nim-1908.go
+++ leetcode/editor/cn/game-of-nim-1908.go
@@ -1,9 +1,11 @@
 package main
 
-//Alice 和 Bob 交替进行一个游戏，由 Alice 先手。
+import "fmt"
+
+// Alice 和 Bob 交替进行一个游戏，由 Alice 先手。
 //
 // 在游戏中，共有 n 堆石头。在每个玩家的回合中，玩家需要 选择 任一非空石头堆，从中移除任意 非零 数量的石头。如果不能移除任意的石头，就输掉游戏，同时另
-//一人获胜。
+// 一人获胜。
 //
 // 给定一个整数数组 piles ，piles[i] 为 第 i 堆石头的数量，如果 Alice 能获胜返回 true ，反之返回 false 。
 //
@@ -14,35 +16,35 @@ package main
 // 示例 1：
 //
 //
-//输入：piles = [1]
-//输出：true
-//解释：只有一种可能的情况：
-//- 第一回合，Alice 移除了第 1 堆中 1 块石头。piles = [0]。
-//- 第二回合，Bob 没有任何石头可以移除。Alice 获胜。
+// 输入：piles = [1]
+// 输出：true
+// 解释：只有一种可能的情况：
+// - 第一回合，Alice 移除了第 1 堆中 1 块石头。piles = [0]。
+// - 第二回合，Bob 没有任何石头可以移除。Alice 获胜。
 //
 //
 // 示例 2：
 //
 //
-//输入：piles = [1,1]
-//输出：false
-//解释：可以证明，Bob一定能获胜。一种可能的情况：
-//- 第一回合，Alice 移除了第 1 堆中 1 块石头。 piles = [0,1]。
-//- 第二回合，Bob 移除了第 2 堆中 1 块石头。 piles = [0,0]。
-//- 第三回合，Alice 没有任何石头可以移除。Bob 获胜。
+// 输入：piles = [1,1]
+// 输出：false
+// 解释：可以证明，Bob一定能获胜。一种可能的情况：
+// - 第一回合，Alice 移除了第 1 堆中 1 块石头。 piles = [0,1]。
+// - 第二回合，Bob 移除了第 2 堆中 1 块石头。 piles = [0,0]。
+// - 第三回合，Alice 没有任何石头可以移除。Bob 获胜。
 //
 //
 // 示例 3：
 //
 //
-//输入：piles = [1,2,3]
-//输出：false
-//解释：可以证明，Bob一定能获胜。一种可能的情况：
-//- 第一回合，Alice 移除了第 3 堆中 3 块石头。 piles = [1,2,0]。
-//- 第二回合，Bob 移除了第 2 堆中 1 块石头。 piles = [1,1,0]。
-//- 第三回合，Alice 移除了第 1 堆中 1 块石头。piles = [0,1,0]。
-//- 第四回合，Bob 移除了第 2 堆中 1 块石头。 piles = [0,0,0]。
-//- 第三回合，Alice 没有任何石头可以移除。Bob 获胜。
+// 输入：piles = [1,2,3]
+// 输出：false
+// 解释：可以证明，Bob一定能获胜。一种可能的情况：
+// - 第一回合，Alice 移除了第 3 堆中 3 块石头。 piles = [1,2,0]。
+// - 第二回合，Bob 移除了第 2 堆中 1 块石头。 piles = [1,1,0]。
+// - 第三回合，Alice 移除了第 1 堆中 1 块石头。piles = [0,1,0]。
+// - 第四回合，Bob 移除了第 2 堆中 1 块石头。 piles = [0,0,0]。
+// - 第三回合，Alice 没有任何石头可以移除。Bob 获胜。
 //
 //
 //
@@ -61,7 +63,7 @@ package main
 // Related Topics 位运算 脑筋急转弯 数组 数学 动态规划 博弈 👍 11 👎 0
 
 // 2022-12-31 17:28:27
-//leetcode submit region begin(Prohibit modification and deletion)
+// leetcode submit region begin(Prohibit modification and deletion)
 func nimGame1(piles []int) bool {
 	xor := 0
 	for _, v := range piles {
@@ -75,7 +77,7 @@ func nimGame1(piles []int) bool {
 func nimGame(piles []int) bool {
 	// leetcode submit region end(Prohibit modification and deletion)
 	mask := 0
-	for i, v := range piles {
+	for _, v := range piles {
 		mask = (mask << 3) + v
 	}
 	memo := make(map[int]bool)
@@ -87,22 +89,29 @@ func nimGame(piles []int) bool {
 			return v
 		}
 		for i := 0; i < len(piles); i++ {
-			t := (mask >> (3 * i)) & 7
+			t := (n >> (3 * i)) & 7
 			if t == 0 {
 				continue
 			}
 			for j := 1; j <= t; j++ {
 				a := j << (3 * i)
-				if mask-a == 0 || !play(mask-a) {
-					memo[mask] = true
+				if n-a == 0 || !play(n-a) {
+					memo[n] = true
 					return true
 				}
 			}
 		}
-		memo[mask] = false
+		memo[n] = false
 		return false
 	}
 
 	return play(mask)
 
+}
+
+// leetcode submit region end(Prohibit modification and deletion)
+func main() {
+	// fmt.Println(nimGame([]int{1}))
+	fmt.Println(nimGame([]int{1, 1}))
+	fmt.Println(nimGame([]int{1, 2, 3}))
 }
