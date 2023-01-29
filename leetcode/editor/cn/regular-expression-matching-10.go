539396608b37e7dffedbd403ed157a329550ab17
cbweixin <cbweixin@qq.com>
2023-01-28 20:20:52 -0800

    > 2023/01/28 20:20:22 Success: 	Runtime:4 ms, faster than 47.63% of Go online submissions. 	Memory Usage:2.7 MB, less than 11.42% of Go online submissions.

--- leetcode/editor/cn/regular-expression-matching-10.go
+++ leetcode/editor/cn/regular-expression-matching-10.go
@@ -52,7 +52,7 @@ import "fmt"
 //2023-01-28 18:53:05
 
 // leetcode submit region begin(Prohibit modification and deletion)
-func isMatch(s string, p string) bool {
+func isMatch2(s string, p string) bool {
 	m, n := len(s), len(p)
 	dp := make([][]bool, m+1)
 	for i := 0; i < m+1; i++ {
@@ -80,6 +80,36 @@ func isMatch(s string, p string) bool {
 		}
 	}
 	return dp[m][n]
+}
+
+func isMatch(s string, p string) bool {
+	m, n := len(s), len(p)
+	dp := make([][]int, m+1)
+	for i := 0; i < m+1; i++ {
+		dp[i] = make([]int, n+1)
+	}
+	dp[0][0] = 1
+	for j := 2; j < n+1; j++ {
+		if p[j-1] == '*' {
+			dp[0][j] = dp[0][j-2]
+		}
+	}
+
+	for i := 1; i < m+1; i++ {
+		for j := 1; j < n+1; j++ {
+			if p[j-1] != '*' {
+				if s[i-1] == p[j-1] || p[j-1] == '.' {
+					dp[i][j] = dp[i-1][j-1]
+				}
+			} else {
+				dp[i][j] = dp[i][j-2]
+				if s[i-1] == p[j-2] || p[j-2] == '.' {
+					dp[i][j] |= dp[i-1][j]
+				}
+			}
+		}
+	}
+	return dp[m][n] == 1
 }
 
 // leetcode submit region end(Prohibit modification and deletion)
