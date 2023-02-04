f57d4949d98bce0d06656919117a0b6820e1fbf6
cbweixin <cbweixin@qq.com>
2023-02-04 12:00:20 -0800

    > 2023/02/04 12:00:15 Success: 	Runtime:48 ms, faster than 86.41% of Go online submissions. 	Memory Usage:7.1 MB, less than 44.31% of Go online submissions.

--- leetcode/editor/cn/sum-of-subarray-minimums-907.go
+++ leetcode/editor/cn/sum-of-subarray-minimums-907.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 //给定一个整数数组 arr，找到 min(b) 的总和，其中 b 的范围为 arr 的每个（连续）子数组。
 //
 // 由于答案可能很大，因此 返回答案模 10^9 + 7 。
@@ -45,7 +47,7 @@ func sumSubarrayMins(arr []int) int {
 	tArr = append(tArr, 0)
 	mod, res := 1000000007, 0
 	for i, v := range tArr {
-		for len(st) > 0 && st[len(st)-1] > v {
+		for len(st) > 0 && tArr[st[len(st)-1]] > v {
 			j := st[len(st)-1]
 			st = st[:len(st)-1]
 			k := st[len(st)-1]
@@ -59,4 +61,7 @@ func sumSubarrayMins(arr []int) int {
 
 }
 
-//leetcode submit region end(Prohibit modification and deletion)
+// leetcode submit region end(Prohibit modification and deletion)
+func main() {
+	fmt.Println(sumSubarrayMins([]int{3, 1, 2, 4}))
+}
