b989a95b72933a3541ff2611a86eeac964bfb7ae
cbweixin <cbweixin@qq.com>
2023-05-06 17:17:14 -0700

    > 2023/05/06 17:17:03 Success: 	Runtime:148 ms, faster than 20.00% of Go online submissions. 	Memory Usage:51.4 MB, less than 10.00% of Go online submissions.

--- leetcode/editor/cn/number-of-sets-of-k-non-overlapping-line-segments-1621.go
+++ leetcode/editor/cn/number-of-sets-of-k-non-overlapping-line-segments-1621.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 //给你一维空间的 n 个点，其中第 i 个点（编号从 0 到 n-1）位于 x = i 处，请你找到 恰好 k 个不重叠 线段且每个线段至少覆盖两个点的方案数
 //。线段的两个端点必须都是 整数坐标 。这 k 个线段不需要全部覆盖全部 n 个点，且它们的端点 可以 重合。
 //
@@ -83,16 +81,13 @@ func get_combin(n int, m int) int {
 		} else if cache[n][k] != -1 {
 			return cache[n][k]
 		} else {
-			cache[n][k] = (combine(n-1, k-1) + combine(n-1, k)) % mod
+			cache[n][k] = (combine(n-1, k) + combine(n-1, k-1)) % mod
 		}
 
 		return cache[n][k]
 	}
 
-	t := combine(n, m)
-	//return combine(n, m)
-	//fmt.Println("%v", cache)
-	return t
+	return combine(n, m)
 
 }
 
@@ -101,6 +96,7 @@ func numberOfSets(n int, k int) int {
 }
 
 // leetcode submit region end(Prohibit modification and deletion)
-func main() {
-	fmt.Println(numberOfSets(4, 2))
-}
+
+//func main() {
+//	fmt.Println(numberOfSets(4, 2))
+//}
